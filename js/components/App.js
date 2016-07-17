da115ae0278e249a707661b307189e8cc7445131
Ken Zhang <kyz211@nyu.edu>
2016-07-17 19:22:38 -0400

    initial commit

---
+++ js/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Relay from 'react-relay';
+
+class App extends React.Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <h1>Test</h1>
+        <ul>
+          {this.props.viewer.widgets.edges.map(edge =>
+            <li key={edge.node.id}>{edge.node.name} (ID: {edge.node.id})</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Relay.createContainer(App, {
+  fragments: {
+    viewer: () => Relay.QL`
+      fragment on User {
+        widgets(first: 10) {
+          edges {
+            node {
+              id,
+              name,
+            },
+          },
+        },
+      }
+    `,
+  },
+});
